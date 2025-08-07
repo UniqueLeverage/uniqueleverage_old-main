ed3920d074966c9cabeedf69473787f1ba286da9
poohtipong <vmalolin@gmail.com>
2025-08-08 06:37:34 +0700

    for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT ? Number(process.env.PORT) : 4173,
+    host: true,
+    allowedHosts: ['ul-new-app-main.onrender.com']  // 👈 Add this line
+  }
 })
